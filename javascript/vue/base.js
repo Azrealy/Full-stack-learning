3c098a7c28ffc39c8f2ab3e56dae54d1f75e5f8e
Azrealy <fangkaihang1992@yahoo.co.jp>
2018-09-09 21:40:36 +0900

    update base.js

---
+++ javascript/vue/base.js
@@ -0,0 +1,30 @@
+<script>
+  var bin = true
+  var str = "hello world"
+  var num = 3.14
+  console.log(bin)
+  console.log(str)
+  console.log(num)
+  var arr = [
+    "helllo",
+    "hello world"
+  ]
+  console.log(arr[1])
+  console.log(arr[0])
+
+  var dict = {
+    user: 'alice',
+    age: 28
+  }
+  console.log(dict['user'])
+  console.log(dict.age)
+  var userConnection = {
+    user: 'john',
+    firends: {
+        number: 20,
+      collection: ['alice', 'alex']
+    }
+  }
+  console.log(userConnection.firends.number)
+  console.log(userConnection.firends.collection)
+</script>
